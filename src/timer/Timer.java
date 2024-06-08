f5f1510a7e52543bdc02cf39d19f231f13b2c982
RamyAdell <ramyadelshawky@hotmail.com>
2024-06-08 09:12:28 +0300

    Java

---
+++ src/timer/Timer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package timer;
+
+/**
+ *
+ * @author Ramy
+ */
+public class Timer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame s = new NewJFrame();
+        s.setVisible(true);
+        s.setLocationRelativeTo(null);
+    }
+    
+}
